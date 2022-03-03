f5b698875f5218c14fc0747dc2681a5112ab3e23
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-03 15:08:31 +0100

    Aggiunti i commenti es paLi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+//--------------------------PRIMO ESERCIZIO--------------------\\
+
+//chiedo all'utente di iserire una parola e creo una costante per il risultato del ciclo
+
 const userPhrase = prompt('inserisci una parola');
 let phrasePalindroma = '';
-
+//sapendo che promp ci torna una stringa controllo ogni lettera di indice [i] e la confronto con la sua opposta
 for(let i = 0; i < userPhrase.length; i++){
     if( userPhrase[i] == userPhrase[(userPhrase.length - i) - 1]){
         phrasePalindroma = true;
@@ -8,12 +12,14 @@ for(let i = 0; i < userPhrase.length; i++){
         phrasePalindroma = false;
     }   
 }
-
+//avendo ora un valore booleano stampo in console se è palindoma o no
 if( phrasePalindroma == true){
     console.log('è palindroma');
 }else{
     console.log('non è palindroma');
 }
 
-//------------SECONDO ESERCIZIO---------------\\
+//-------------------------SECONDO ESERCIZIO--------------------\\
+
+
 
